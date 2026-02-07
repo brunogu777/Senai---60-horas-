c8b0ca5bbe3c5c125a3fefe3feeb052bac2c43e6
brunogu777 <brunogu2830@gmail.com>
2026-02-07 11:44:47 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,32 @@
+# 0
+numeros = list(range(2,21,2))
+print(numeros)
+
+
+# 1
+numeros = list(range(1,11))
+print(numeros) 
+
+
+# 2
+print(numeros[2])
+
+
+# 3
+numeros.append(9)
+print(numeros)
+
+
+# 4
+numeros.remove(5)
+print(numeros)
+
+
+# 5
+carros = ['jeep','ferrari','fusca']
+print(carros , numeros)
+
+
+carros+=(numeros)
+print(carros)
+
